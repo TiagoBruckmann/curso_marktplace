b005c1f7b57ad602678ffaefd90885806ffb0e47
Tiago Bruckmann <tiago_bruck@outlook.com.br>
2020-09-23 17:14:03 -0300

    criação de factors migrations e mais

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/model', function(){
+    // $products = \App\Product::all();  //select * from products
+    // return $products;
+
+    /*
+    $user = new \App\User();
+    $user = \App\User::find(1);
+    $user->name = 'usuario teste editado';
+    $user->save();
+    return $user->save();
+    */
+
+    // \App\User::all(); - retorna todos os usuarios
+    // \App\User::find(3); - retorna o usuario de acordo com seu id
+    // \App\User::where('name', 'nome a ser pesquisado')->get(); - retorna o usuario de acordo a pesquisa feita
+    // return \App\User::paginate(10); paginar dados por pagina
+
+    return \App\User::all();
+    
+});
