b66ab0d0f5b4aabed83a7dbcab8208c6da5bf1e2
Tiago Bruckmann <tiago_bruck@outlook.com.br>
2020-10-02 17:31:13 -0300

    adiçao do menu e verificaçao do logon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,32 @@
     <script src="https://kit.fontawesome.com/a25e74ad99.js" crossorigin="anonymous"></script>
 </head>
 <body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="{{ route('admin.stores.index') }}">Lojas <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('admin.products.index') }} ">Produtos</a>
+            </li>
+        </ul>
+        <div>
+            <ul>
+                <li>
+                    <a class="nav-link" href="#">Sair</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    </nav>
+
     <div class="container">
         @include('flash::message')
         @yield('content')
